6ec7d6ee3dcd1b429d5c9f16514c700a7b0ddb73
goldhillpaladin <qiansheng870601@163.com>
2018-04-04 18:32:09 +0800

    add singleton pattern

---
+++ singleton.cpp
@@ -0,0 +1,60 @@
+#ifdef _WIN32
+#include <Windows.h>
+void MySleep(unsigned long milliseconds) {
+	Sleep(milliseconds);
+}
+#elif __linux__
+#include <unistd.h>
+void MySleep(unsigned long milliseconds) {
+	usleep(milliseconds * 1000);
+}
+#endif
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+class Singleton {
+public:
+	static Singleton* GetInstance();
+
+private:
+	Singleton() { std::cout << "Singleton constructor" << std::endl; }
+
+	static std::mutex mut;
+	static Singleton* instance;
+};
+
+std::mutex Singleton::mut;
+Singleton* Singleton::instance = nullptr;
+
+Singleton* Singleton::GetInstance() {
+	if (!instance) {
+		mut.lock();
+		if (!instance)
+			instance = new Singleton();
+		mut.unlock();
+	}
+	
+	return instance;
+}
+
+std::mutex mut;
+
+void fun(int id) {
+	auto instance = Singleton::GetInstance();
+	mut.lock();
+	MySleep(100);
+	std::cout << "#" << std::dec << id;
+	std::cout << " instance = " << std::hex << instance << std::endl;
+	mut.unlock();
+}
+
+int main() {
+	std::vector<std::thread> ths;
+	for (int i = 0; i < 10; ++i)
+		ths.push_back(std::thread(fun, i));
+	for (int i = 0; i < 10; ++i)
+		ths[i].join();
+}
